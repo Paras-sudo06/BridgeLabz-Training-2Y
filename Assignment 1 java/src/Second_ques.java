4b6faaaaa3d13da06c6a83917953800efe57bb1c
Paras-sudo06 <paras.shukla.dev@gmail.com>
2025-08-12 14:46:05 +0530

    Assignment 1 java

---
+++ Assignment 1 java/src/Second_ques.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Second_ques {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int a = sc.nextInt();
+		System.out.println("Enter second number");
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.println("The sum of two number is:"+sum);
+	}
+
+}
---
+++ Assignment 1 java/src/Thirs_Ques.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Thirs_Ques {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the celcius temprature");
+		float a = sc.nextInt();
+		float farenheit = (a * 9/5) + 32;
+		System.out.println("The farenheit temprature is:"+farenheit);
+
+	}
+
+}
